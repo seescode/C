255fd569db3ec99eb68a338ee22b123ebe157a7d
seescode <seesyong@gmail.com>
2016-02-02 11:24:02 -0500

    Remove bower components from source control

--- bower_components/super-alert/dist/templates.js
+++
@@ -1,8 +0,0 @@
-angular.module('atom.superalert').run(['$templateCache', function($templateCache) {
-  'use strict';
-
-  $templateCache.put('/directives/superAlert/superAlert.html',
-    "<button ng-click=\"click()\">Alert!</button>"
-  );
-
-}]);
--- bower_components/super-alert/grunt/clean.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	"clean-dist": {
-		src: [
-			'dist/*'
-		]
-	}
-};
